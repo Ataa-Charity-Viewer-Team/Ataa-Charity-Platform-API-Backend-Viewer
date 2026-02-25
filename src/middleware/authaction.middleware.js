799f75e3d90935abf08c96dd9fec5bf8693b7e80
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-02-25 23:52:17 +0200

    auth

--- src/middleware/authaction.middleware.js
+++ src/middleware/authaction.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { userModel } from "../database/model/user.model.js";
+import { userModel } from "../../../database/model/user.model.js"; // ← صحح المسار
 
 const authAction = async (req, res, next) => {
   const { authorization } = req.headers;
@@ -9,30 +9,31 @@ const authAction = async (req, res, next) => {
   }
 
   let decoded;
-    decoded = jwt.decode(authorization);
-  } 
+
+  // ✓ decode الأول عشان نجيب الـ id
+  decoded = jwt.decode(authorization);
 
   if (!decoded?.id) {
     return next(new Error("invalid token", { cause: 401 }));
   }
 
   const user = await userModel.findById(decoded.id);
+
   if (!user) {
     return next(new Error("unauthorized user not found", { cause: 401 }));
   }
 
+  // ✓ verify مع try/catch
     jwt.verify(
       authorization,
       process.env.ACCESS_SECRET + user.updatedAt.getTime()
     );
-  
-
+  } 
   if (user.verify === false) {
     return next(new Error("verify your account", { cause: 401 }));
+    
+    req.user = user;
+    return next();
   }
-
-  req.user = user;
-  return next();
-
 
 export default authAction;
