bd4626d5e11d11e53a2bd0fa3a6da1b105431c43
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-02-25 17:27:55 +0200

    Herzallah

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // import express from "express";
-// import { bootstrap } from "./src/app.controller.js";
 // const app = express();
 // app.use(express.json()); 
 //   // ================= Test API (مهم جداً للـ Vercel) =================
 //   app.get("/", (req, res) => {
-//     res.json({ message: "API is running 🚀 By Eng. Sayed Herzallah" });
-//   });
+  //     res.json({ message: "API is running 🚀 By Eng. Sayed Herzallah" });
+  //   });
 
 // bootstrap(app, express);
 import serverless from "serverless-http";
