53f1d559a3bcfd16793cd9665ba8a27622471028
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-02-25 12:21:24 +0200

    🚀 Upload Project Back End Developer By Sayed Herzallah

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import dotenv from "dotenv"
 dotenv.config()
-import  express  from "express"
+import express from "express"
 import { bootstrap } from "./src/app.controller.js"
+
 const app = express()
-const port = process.env.PORT ||  8000
-await bootstrap(app,express)
+const port = process.env.PORT || 8000
 
+const startServer = async () => {
+  await bootstrap(app, express)
+  app.listen(port, () => console.log("server is running on port"))
+}
 
-app.listen(port, () => {console.log("server is running on port");
-})
+startServer()
+
+export default app
