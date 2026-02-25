46ff9ff951426707be7c0343423edc2296fc0fb3
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-02-25 11:49:36 +0200

    fix: remove env from git

--- src/Middleware/express.limit.middleware.js
+++ src/Middleware/express.limit.middleware.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000,
-  max: 100,
+  max: 20,
   standardHeaders: true,
   legacyHeaders: false,
   handler: (req, res) => {
