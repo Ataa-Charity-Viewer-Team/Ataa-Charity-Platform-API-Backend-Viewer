1032780dc7068d60eabd590559b4273a2f6ee96d
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-02-25 18:29:54 +0200

    fix error connect

--- src/database/connect.js
+++ src/database/connect.js
@@ -2,11 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("DB connected successfully ✅");
   } catch (error) {
     console.error("Failed to connect DB ❌", error.message);
