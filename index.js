3f8e2343fe3e8695494971914475d5cc9f328238
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-02-26 00:49:16 +0200

    server

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-// import express from "express";
-// import { bootstrap } from "./src/app.controller.js";
-
-// const app = express();
-// app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   res.json({ message: "API is running 🚀" });
-// });
-
-// await bootstrap(app, express);
-
-// export default app;
 import express from "express";
 import { bootstrap } from "./src/app.controller.js";
 
 const app = express();
+app.use(express.json());
 
-await bootstrap(app, express);
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.get("/", (req, res) => {
+  res.json({ message: "API is running 🚀" });
 });
 
+await bootstrap(app, express);
+
 export default app;
+
+// import express from "express";
+// import { bootstrap } from "./src/app.controller.js";
+
+// const app = express();
+
+// await bootstrap(app, express);
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
+
+// export default app;
