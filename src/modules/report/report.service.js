a468da44d0305ea5c89a18a281b123a13c38d101
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-02-27 01:58:52 +0200

    update repet

--- src/modules/report/report.service.js
+++ src/modules/report/report.service.js
@@ -3,7 +3,7 @@ import { advancedPagination } from "../../middleware/pagination.middleware.js";
 
 // =================================== 1) Get All Reports ================================
 export const getAllReports = async (req, res, next) => {
-  const data = await advancedPagination(reportModel, {}, req);
+  const data = await advancedPagination(reportModel);
   return res.status(200).json({ success: true, data });
 };
 // =================================== 2) Create Report ================================
@@ -11,7 +11,7 @@ export const createReport = async (req, res, next) => {
   const { description, type } = req.body;
   const { user } = req;
   const existing = await reportModel.findOne({ adminId: req.user._id });
-  if (!existing) {
+  if (existing) {
     return next(new Error("not reported", { cause: 400 }));
   }
   const report = await reportModel.create({ adminId: user._id, description, type });
