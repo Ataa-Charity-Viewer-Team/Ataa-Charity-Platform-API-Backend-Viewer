878b0ec4a9536fe577b87c5271e2170dab8b53e0
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-02-26 17:21:17 +0200

    Send Email Vercel

--- src/utils/sendemails/emailevent.js
+++ src/utils/sendemails/emailevent.js
@@ -1,12 +1,13 @@
-import { EventEmitter } from "events";
-import {sendEmails } from "./sendemail.nodemailer.js";
+// import { EventEmitter } from "events";
+// import {sendEmails } from "./sendemail.nodemailer.js";
 
-export const emailEmmiter = new EventEmitter();
-emailEmmiter.on("sendEmail", async (data) => {
-    await sendEmails({
-      to: data.to,
-      subject: data.subject,
-      html: data.html,
-    });
+// export const emailEmmiter = new EventEmitter();
+// emailEmmiter.on("sendEmail", async (data) => {
+//     await sendEmails({
+//       to: data.to,
+//       subject: data.subject,
+//       html: data.html,
+//     });
 
-});
+// });
+
