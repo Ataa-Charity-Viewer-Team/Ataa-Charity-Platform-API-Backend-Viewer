8ee4459644a8efac9e1b63ac77c05e2209c4c37e
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-03-06 03:29:26 +0200

    Update Project

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,140 +1,3 @@
-// import { userModel } from "../../database/model/user.model.js";
-// import { encryptPhone } from "../../utils/encryption/encryption.js";
-// import { hashPassword, comparePassword } from "../../utils/hashing/hashing.js";
-// import { createToken, verifyToken } from "../../utils/token/token.js";
-// import { customAlphabet } from "nanoid";
-// import { emailEmmiter } from '../../utils/sendemails/emailevent.js';
-// import { templet } from '../../utils/sendemails/generate.html.js';
-// import { codeOTP, otpModel } from "../../database/model/otp.model.js";
-// // ===========================1) Register Account  ===========================
-// export const registerAccount = async (req, res, next) => {
-//   const { userName, email, phone, password, address } = req.body;
-
-//   const existingUser = await userModel.findOne({ email });
-//   if (existingUser) {
-//     return next(new Error("Email already exists", { cause: 409 }));
-//   }
-//   const passwordHash = hashPassword({ plainText: password });
-//   const encryptedPhone = encryptPhone({ cipherText: phone });
-//   const newUser = await userModel.create({ ...req.body, phone: encryptedPhone, password: passwordHash });
-//   const userData = await userModel.findById(newUser._id).select("-password -__v -phone");
-//   const sendCode = customAlphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", 6)();
-//   await otpModel.deleteMany({ userId: newUser._id, codeType: codeOTP.activateAccount });
-//   await otpModel.create({ userId: newUser._id, code: sendCode, codeType: codeOTP.activateAccount });
-//   emailEmmiter.emit("sendEmail", { to: email, subject: codeOTP.activateAccount, html: templet({ sendCode: sendCode }) });
-//   return res.status(201).json({ success: true, user: userData, message: "User registered successfully. Please check your email for OTP." });
-// };
-
-// // ===========================2) Login  ===========================
-// export const login = async (req, res, next) => {
-//   const { email, password } = req.body;
-
-//   const user = await userModel.findOne({ email });
-//   if (!user) return next(new Error("Invalid email or password", { cause: 404 }));
-
-//   const isValid = comparePassword({ plainText: password, hashPassword: user.password });
-//   if (!isValid) {
-//     return next(new Error("Invalid email or password", { cause: 401 }));
-//   }
-
-//   if (!user.verify) {
-//     return next(new Error("Please verify your email first", { cause: 403 }));
-//   }
-
-//   const accessToken = createToken({
-//     payload: { id: user._id, roleType: user.roleType },
-//     secret: process.env.ACCESS_SECRET + user.createdAt.getTime(),
-//     options: { expiresIn: process.env.ACCESS_TOKEN } //7h
-//   });
-//   const refreshToken = createToken({
-//     payload: { id: user._id, roleType: user.roleType },
-//     secret: process.env.REFRESH_SECRET,
-//     options: { expiresIn: process.env.REFRESH_TOKEN } //50h
-//   });
-//   return res.status(200).json({
-//     success: true, message: "User logged in successfully",
-//     tokens: { accessToken, refreshToken }
-//   });
-// };
-
-// // ===========================3) Verify Email ===========================
-// export const verifyEmail = async (req, res, next) => {
-//   const { email, code } = req.body;
-
-//   const user = await userModel.findOne({ email });
-//   if (!user) return next(new Error("User not found", { cause: 404 }));
-//   if (user.verify) {
-//     return next(new Error("Email already verified", { cause: 409 }));
-//   }
-//   const otpRecord = await otpModel.findOne({ userId: user._id, code, codeType: codeOTP.activateAccount });
-//   if (!otpRecord) {
-//     return next(new Error("Invalid or Expired OTP", { cause: 400 }));
-//   }
-//   user.verify = true;
-//   await user.save();
-//   await otpModel.deleteOne({ _id: otpRecord._id });
-//   return res.status(200).json({
-//     success: true,
-//     message: "Email verified successfully. You can login now."
-//   });
-// };
-
-// // ====================4) Forget Password  =======================
-// export const forgetPassword = async (req, res, next) => {
-//   const { email } = req.body;
-//   const user = await userModel.findOne({ email });
-//   if (!user) {
-//     return next(new Error("User not found", { cause: 404 }));
-//   }
-//   await otpModel.deleteMany({ userId: user._id, codeType: codeOTP.forgetPassword });
-//   const sendForget = customAlphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", 6)();
-//   await otpModel.create({ userId: user._id, code: sendForget, codeType: codeOTP.forgetPassword });
-//   emailEmmiter.emit("sendEmail", { to: email, subject: codeOTP.forgetPassword, html: templet({ sendCode: sendForget }) });
-
-//   return res.status(200).json({
-//     success: true,
-//     message: "Password reset OTP sent to your email."
-//   });
-// };
-
-// // ===========================5) Reset Password ===========================
-// export const resetPassword = async (req, res, next) => {
-//   const { email, code, password } = req.body;
-
-//   const user = await userModel.findOne({ email });
-//   if (!user) {
-//     return next(new Error("User not found", { cause: 404 }));
-//   }
-//   const otpRecord = await otpModel.findOne({ userId: user._id, code, codeType: codeOTP.forgetPassword });
-//   if (!otpRecord) {
-//     return next(new Error("Invalid or Expired OTP", { cause: 400 }));
-//   }
-//   user.password = hashPassword({ plainText: password });
-//   await user.save();
-//   await otpModel.deleteOne({ _id: otpRecord._id });
-//   return res.status(200).json({ success: true, message: "Password has been reset successfully." });
-// };
-
-// // ===========================6) Refresh Token ===========================
-// export const refreshToken = async (req, res, next) => {
-//   const { refreshToken } = req.body;
-
-//   const decoded = verifyToken({ token: refreshToken, secret: process.env.REFRESH_SECRET });
-//   if (!decoded?.id) {
-//     return next(new Error("Invalid Token", { cause: 400 }));
-//   }
-//   const user = await userModel.findById(decoded.id);
-//   if (!user) {
-//     return next(new Error("User not found", { cause: 404 }));
-//   }
-
-//   const newAccessToken = createToken({
-//     payload: { id: user._id, roleType: user.roleType },
-//     secret: process.env.ACCESS_SECRET + user.createdAt.getTime(),
-//     options: { expiresIn: process.env.ACCESS_TOKEN }
-//   });
-//   return res.status(200).json({ success: true, accessToken: newAccessToken });
-// };
 
 // ========================== # Auth Service and send email vercel # =======================
 import { userModel } from "../../database/model/user.model.js";
@@ -145,12 +8,24 @@ import { customAlphabet } from "nanoid";
 import { sendEmails } from '../../utils/sendemails/sendemail.nodemailer.js';
 import { templet } from '../../utils/sendemails/generate.html.js';
 import { codeOTP, otpModel } from "../../database/model/otp.model.js";
+import BlockedUser from "../../database/model/blockuser.model.js";
 import { waitUntil } from '@vercel/functions';
 
 // ===========================1) Register Account  ===========================
 export const registerAccount = async (req, res, next) => {
   const { userName, email, phone, password, address } = req.body;
+const ip = req.headers['x-forwarded-for']?.split(',')[0].trim() 
+           || req.socket.remoteAddress;
 
+  const isBlocked = await BlockedUser.findOne({
+    $or: [
+      { ipAddress: ip },
+      { email }
+    ]
+  });
+ if (isBlocked) {
+    return next(new Error("You are blocked by the Salek team for security reasons and to protect your privacy and security. please contact us for more information", { cause: 403 }));
+  }
   const existingUser = await userModel.findOne({ email });
   if (existingUser) {
     return next(new Error("Email already exists", { cause: 409 }));
@@ -173,7 +48,18 @@ export const registerAccount = async (req, res, next) => {
 // ===========================2) Login  ===========================
 export const login = async (req, res, next) => {
   const { email, password } = req.body;
+const ip = req.headers['x-forwarded-for']?.split(',')[0].trim() 
+           || req.socket.remoteAddress;
 
+  const isBlocked = await BlockedUser.findOne({
+    $or: [
+      { ipAddress: ip },
+      { email }
+    ]
+  });
+ if (isBlocked) {
+    return next(new Error("You are blocked by the Salek team for security reasons and to protect your privacy and security. please contact us for more information", { cause: 403 }));
+  }
   const user = await userModel.findOne({ email });
   if (!user) return next(new Error("Invalid email or password", { cause: 404 }));
 
