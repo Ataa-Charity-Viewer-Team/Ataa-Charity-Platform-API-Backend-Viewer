b85d89e7f374e59f084cf47793de9594b5dc67fa
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-04-02 22:07:32 +0200

    Finish Update Project

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,10 +1,8 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  console.log("MONGO_URI:", process.env.MONGO_URI);
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("DB connected successfully ✅");
+    await mongoose.connect(process.env.MONGO_URI.replace("MONGO_URI=", ""));
   } catch (error) {
     console.error("Failed to connect DB ❌", error.message);
     throw error;
