544baafdfdf80e778ffd9a7fdd6fa1b95370725e
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-04-03 10:55:43 +0200

    Update Project

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,30 +1,14 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  const MONGO_URI = process.env.MONGO_URI;
-
-  const connect = async () => {
-    try {
-      await mongoose.connect(MONGO_URI, {
-        serverSelectionTimeoutMS: 5000,
-      });
-
-      console.log("MongoDB Connected ✅");
-    } catch (error) {
-      console.error("DB connection failed ❌, retrying in 5s...");
-      setTimeout(connect, 5000); // retry
-    }
-  };
-
-  await connect();
-  mongoose.connection.on("disconnected", () => {
-    console.log("MongoDB disconnected ⚠️... reconnecting");
-    connect();
-  });
-
-  mongoose.connection.on("error", (err) => {
-    console.log("MongoDB error ❌", err.message);
-  });
+  console.log("MONGO_URI:", process.env.MONGO_URI);
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB connected successfully ✅");
+  } catch (error) {
+    console.error("Failed to connect DB ❌", error.message);
+    throw error;
+  }
 };
 
 export default connectDB;
