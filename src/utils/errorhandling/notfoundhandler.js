24a4ee519cf3502116d5149951c69b6973a6998b
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-04-07 16:17:56 +0200

    Update function Not Found Handler

--- src/utils/errorhandling/notfoundhandler.js
+++ src/utils/errorhandling/notfoundhandler.js
@@ -1,4 +1,4 @@
 // ================================= Not Found Handler ===================================
 export const notFoundHandler = (req, res, next) => {
-  return next(new Error(`Route ${req.originalUrl} not found`, { cause: 404 }))
+  return next(new Error(`Route ${req.originalUrl} not found in this server `, { cause: 404 }))
 }
