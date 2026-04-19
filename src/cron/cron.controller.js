6d2e08cc9186832dede44a0bd8f1052cfd4ed26d
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-04-19 09:16:28 +0200

    Update Cron controller and services

--- src/cron/cron.controller.js
+++ src/cron/cron.controller.js
@@ -56,19 +56,19 @@
 
 import { Router } from "express";
 import { asyncHandler } from "../utils/errorhandling/asynchandler.js";
-import * as cronController from "./cron.services.js";
+import * as cronServices from "./cron.services.js";
 import authAction from './../middleware/authaction.middleware.js';
 import { authorization } from './../middleware/authorization.middleware.js';
 import { cronEndpoint } from "./cron.endpoint.js";
 
 const router = Router();
 
-router.get("/adminReport",authAction,authorization(cronEndpoint.adminReport),asyncHandler(cronController.adminReport));
+router.get("/adminReport",authAction,authorization(cronEndpoint.adminReport),asyncHandler(cronServices.adminReport));
 
 router.get("/donationReminder",
   authAction,
   authorization(cronEndpoint.donationReminder),
-  asyncHandler(cronController.donationReminder)
+  asyncHandler(cronServices.donationReminder)
 );
 
 export default router;
--- src/cron/cron.services.js
+++ src/cron/cron.services.js
@@ -1,24 +1,17 @@
-// cron.service.js
-
+// ========================== # Cron Service # ==========================
 import { sendAdminReportEmail } from "../utils/automation/admin.report.service.js";
 import { sendPendingDonationReminders } from "../utils/automation/donation.reminder.service.js";
 
-export const adminReport = async () => {
+// ===================== Admin Report =====================
+export const adminReport = async (req, res) => {
   console.log("[Cron] 🕗 Admin Report triggered...");
   await sendAdminReportEmail();
-
-  return {
-    job: "admin-report",
-    message: "Admin report sent successfully",
-  };
+  return res.status(200).json({ success: true, job: "adminReport", message: "Admin report sent successfully." });
 };
 
-export const donationReminder = async () => {
+// ===================== Donation Reminder =====================
+export const donationReminder = async (req, res) => {
   console.log("[Cron] 🕘 Donation Reminder triggered...");
   await sendPendingDonationReminders();
-
-  return {
-    job: "donation-reminder",
-    message: "Donation reminders sent successfully",
-  };
+  return res.status(200).json({ success: true, job: "donationReminder", message: "Donation reminders sent successfully." });
 };
