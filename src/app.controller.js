6b5503ea42c889c5c5926edf047f2279588d110a
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-04-20 20:50:25 +0200

    Remove Admin App Controller

--- src/app.controller.js
+++ src/app.controller.js
@@ -11,7 +11,6 @@ import evalutionRouter from "./modules/evaluation/evalution.controller.js"
 import reportRouter from "./modules/report/report.controller.js"
 import aiRouter from "./modules/ai/ai.controller.js"
 import limiter from "./middleware/express.limit.middleware.js";
-import adminRouter from "./modules/admin/admin.controller.js";
 import helmet from "helmet";
 import notificationRouter from "./modules/notification/notification.controller.js";
 import cors from "cors"
@@ -43,7 +42,6 @@ export const bootstrap = async (app, express) => {
   app.use("/report", reportRouter);
   app.use("/ai", aiRouter);
   app.use("/notification", notificationRouter);
-  app.use("/admin", adminRouter);
   // ======================= import error handlers ============================
   app.use(notFoundHandler);
 
