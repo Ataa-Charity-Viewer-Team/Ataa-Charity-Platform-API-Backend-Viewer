141a8a75953442814fc2934b54185c38bdaa4d22
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-04-22 02:58:08 +0200

    Finish Security

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  console.log("MONGO_URI:", process.env.MONGO_URI);
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("DB connected successfully ✅");
