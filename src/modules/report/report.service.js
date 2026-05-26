8134eb321ea0a27b659a96001e42e9da2fc2012b
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-05-26 16:34:41 +0300

    fix error get all charity report

--- src/modules/report/report.service.js
+++ src/modules/report/report.service.js
@@ -1,4 +1,5 @@
 import { reportModel } from "../../database/model/report.model.js";
+import { charityModel } from "../../database/model/charity.model.js";
 import { userModel } from "../../database/model/user.model.js";
 import { advancedPagination } from "../../middleware/pagination.middleware.js";
 
